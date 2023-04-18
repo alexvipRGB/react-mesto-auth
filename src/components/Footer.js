f45540cf4db478015d55fe2cb55b846bf31108c8
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-04-18 16:12:54 +0300

    Delete Footer.js

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-      <footer className="footer">
-        <p className="footer__author">&copy;&nbsp;2022 Mesto Russia</p>
-      </footer>
-    );
-  }
-  export default Footer;
